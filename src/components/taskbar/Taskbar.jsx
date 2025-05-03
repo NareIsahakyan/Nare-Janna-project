17a980928dfc3397b2a29c70691899cbf46c4c38
naer_isahakyan <nare.isahakjan@mail.ru>
2025-05-04 00:33:28 +0400

    add plusbutton , taskbar.jsx

---
+++ src/components/taskbar/Taskbar.jsx
@@ -0,0 +1,27 @@
+import PlusButton from "../buttons/plus/PlusButton";
+import NewTaskbarAdd from "./NewTaskbarAdd";
+import styles from "./taskbar.module.css";
+import { useTranslation } from "react-i18next";
+const Taskbar = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <div className={styles.taskbarConteiner}>
+        <div className={styles.taskbar}>
+          <div className={styles.taskbarDatas}>
+            <div>
+              <h1>Project Name</h1>
+            </div>
+
+            <div>
+              <NewTaskbarAdd />
+            </div>
+          </div>
+
+          <div className={styles.taskbarTools}></div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Taskbar;
