994057c692c708c2427113f138d6e44b1e0915a5
Jhanna <jhannakhachatryan404@gmail.com>
2025-05-10 14:09:30 +0400

    New json file designed to determine random avatar colors

--- src/pages/dashbordPage/Avatar.jsx
+++ src/pages/dashbordPage/Avatar.jsx
@@ -1,10 +1,32 @@
-import styles from "./Dashboard.module.css"
+/*This is a avatar  */
 
+import { useState } from "react"
+import styles from "./Dashboard.module.css"
+import AvatarJson from "../../json1/Avatra.json"
 function Avatar(){
+     
+    const [avatar,setAvatar]=useState(AvatarJson)
+  
+    
+
     return(
         <>
         <div className={styles.avatar}>
-
+          {avatar[0].avatar1.map((letter,index)=>{
+            <span key={`a1-${index} `} className ={styles.avatar1}>
+                {letter} 
+            </span>
+          })}
+              {avatar[1].avatar2.map((letter,index)=>{
+            <span key={`a2-${index} `} className ={styles.avatar2}>
+                {letter} 
+            </span>
+          })}
+              {avatar[3].avatar3.map((letter,index)=>{
+            <span key={`a3-${index} `} className ={styles.avatar3}>
+                {letter} 
+            </span>
+          })}
         </div>
         </>
     )
